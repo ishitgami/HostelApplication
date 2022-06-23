badf64f7485c614f1957c71863be489043dc07fe
unknown <ishitgami01@gmail.com>
2022-06-23 10:50:07 +0530

    add Registration Screen

---
+++ lib/presentation/screen/auth/registrationScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: const Text('Registration Screen'),
+      ) ,
+    );
+  }
+}
